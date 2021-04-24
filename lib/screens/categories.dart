e1078a22131a07ff94250b736d233919d9f126e3
Aro Micheal - harrowmykel <harrowwestmykel@gmail.com>
2021-04-24 14:25:39 +0200

    added theme font assets and configured data + display

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import '../widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                title: catData.title,
+                color: catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
